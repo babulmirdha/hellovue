84400b0d3ae7e6f85eb77eec719752ee29a79b5e
Iqbal Hossain <iqbalh8622@gmail.com>
2020-07-18 12:00:11 +0600

    fixed test.vue locaion bug

---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>Hello Everyone</h1>
+    
+</template>
+
+<script>
+export default {
+    date(){
+       return{
+           test:[
+            //     {testString: "Read a book", completed: false}
+            //    {testString: "Make a video", completed: true}
+            //    {testString: "Publish a video", completed: false}
+            //    {testString: "Make some food", completed: true}
+
+           ]
+      }  
+    }
+
+    
+}
+</script>
+
+<style>
+
+</style>
